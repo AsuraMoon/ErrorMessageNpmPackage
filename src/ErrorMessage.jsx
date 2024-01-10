39843813cee3de31baf3130db0de5f400f332307
AsuraMoon <73102506+AsuraMoon@users.noreply.github.com>
2024-01-10 11:34:44 +0100

    Initial commit

---
+++ src/ErrorMessage.jsx
@@ -0,0 +1,13 @@
+// ErrorMessage.jsx
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ErrorMessage = ({ message }) => {
+  return <div className="error-message">{message}</div>;
+};
+
+ErrorMessage.propTypes = {
+  message: PropTypes.string.isRequired,
+};
+
+export default ErrorMessage;
